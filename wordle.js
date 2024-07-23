f8a6735648ee57394d8f21272e34b587f4dfd43a
Oscar <oscar.davies109@gmail.com>
2024-07-23 23:18:35 +0100

    Completing files

--- wordle.js
+++ wordle.js
@@ -1,1 +1,19 @@
+const enterButton = document.getElementById('enter');
+enterButton.addEventListener("click", enterFunction)
+ 
+let letterOne = document.getElementById('letter1').value.toUpperCase();
+let letterTwo = document.getElementById('letter2').value.toUpperCase();
+let letterThree = document.getElementById('letter3').value.toUpperCase();
+let letterFour = document.getElementById('letter4').value.toUpperCase();
 
+let correctAwnser = "DUMB";
+
+function enterFunction () {
+    if (letterOne == "D" || letterTwo == "U" || letterThree == "M" || letterFour == "B") {
+        console.log("You have 1 correct letter")
+    }
+
+    if (letterOne == "D" && letterTwo == "U" && letterThree == "M" && letterFour == "B") {
+        console.log("Great job you guessed correctly");
+    }
+}
