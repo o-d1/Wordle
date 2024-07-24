05336808763e0b97f73a14844d0e81f4631cd40c
Oscar <oscar.davies109@gmail.com>
2024-07-24 13:49:40 +0100

    Updating JavaScript and css for wordle site

--- wordle.js
+++ wordle.js
@@ -1,19 +1,41 @@
 const enterButton = document.getElementById('enter');
 enterButton.addEventListener("click", enterFunction)
- 
-let letterOne = document.getElementById('letter1').value.toUpperCase();
-let letterTwo = document.getElementById('letter2').value.toUpperCase();
-let letterThree = document.getElementById('letter3').value.toUpperCase();
-let letterFour = document.getElementById('letter4').value.toUpperCase();
+/*
+function generateRandomString () {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const randomIndex = Math.floor(Math.random() * letters.length);
+    return letters[randomIndex]
+}
+let letter1 = generateRandomString();
+let letter2 = generateRandomString();
+let letter3 = generateRandomString();
+let letter4 = generateRandomString();
+console.log(letter1)
+console.log(letter2)
+console.log(letter3)
+console.log(letter4)
 
-let correctAwnser = "DUMB";
+let correctAwnser = letter1 + letter2 + letter3 + letter4;
+*/
+
+let correct1 = "D";
+let correct2 = "U";
+let correct3 = "M";
+let correct4 = "B";
 
 function enterFunction () {
-    if (letterOne == "D" || letterTwo == "U" || letterThree == "M" || letterFour == "B") {
-        console.log("You have 1 correct letter")
-    }
+    let letterOne = document.getElementById('letter1').value.toUpperCase();
+    let letterTwo = document.getElementById('letter2').value.toUpperCase();
+    let letterThree = document.getElementById('letter3').value.toUpperCase();
+    let letterFour = document.getElementById('letter4').value.toUpperCase();
 
-    if (letterOne == "D" && letterTwo == "U" && letterThree == "M" && letterFour == "B") {
+    if (letterOne == correct1 && letterTwo == correct2 && letterThree == correct3 && letterFour == correct4) {
         console.log("Great job you guessed correctly");
+    } 
+    else if (letterOne == correct1 && (letterTwo !== correct2 || letterThree !== correct3 || letterFour !== correct4)) {
+        console.log("Only " + letterOne + " is correct")
+    } 
+    else {
+        console.log("You're wrong")
     }
 }
